757879d5cbf8ea16de65668a05d3747127f2f832
Elliot Bulmer <sonelliot@gmail.com>
2013-01-25 22:01:49 +0800

    Simple behaviour tree and base entity

---
+++ Spineless/Spineless/Entities/SpinelessEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Gnomic.Entities;
+
+namespace Spineless.Entities
+{
+    public abstract class SpinelessEntity : ClipEntity
+    {
+        
+    }
+}
