1d0af854f9a5d3b6d4a5f9156b083ad75b1de884
Elliot Bulmer <sonelliot@gmail.com>
2013-01-25 22:53:11 +0800

    Added interface class for AI and gave SpinelessEntity physics

--- Spineless/Spineless/Entities/SpinelessEntity.cs
+++ Spineless/Spineless/Entities/SpinelessEntity.cs
@@ -3,12 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+using Gnomic;
 using Gnomic.Entities;
+using Gnomic.Physics;
 
 namespace Spineless.Entities
 {
-    public abstract class SpinelessEntity : ClipEntity
+    public class SpinelessEntitySettings : ClipEntitySettings
     {
-        
+        public PhysicsStructureSettings Physics;
+        [ContentSerializer(Optional = true)]
+        public float MoveForce = 50.0f;
+        [ContentSerializer(Optional = true)]
+        public float MaxSpeed = 0.1f;
+
+        public SpinelessEntitySettings()
+        {
+            this.EntityClass = "Spineless.Entities.SpinelessEntity,Spineless";
+        }
+    }
+
+    public class SpinelessEntity : ClipEntity
+    {
+        new SpinelessEntitySettings Settings;
+
+        protected PhysicsStructure physics;
+
+        public override void Initialize(GameScreen parentScreen)
+        {
+            base.Initialize(parentScreen);
+
+            physics = Settings.Physics.CreateStructure(
+                parentScreen.Physics.World, this.Position);
+            physics.SetVelocityLimit(
+                parentScreen.Physics.World, Settings.MaxSpeed, 0.0f);
+        }
     }
 }
