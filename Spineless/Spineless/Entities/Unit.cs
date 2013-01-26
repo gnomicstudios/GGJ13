65741452088d2a14ddc9a270eca379841e6b0a70
Elliot Bulmer <sonelliot@gmail.com>
2013-01-26 12:11:00 +0800

    Make units moveable

--- Spineless/Spineless/Entities/Unit.cs
+++ Spineless/Spineless/Entities/Unit.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+
+using Spineless.AI;
+
 namespace Spineless.Entities
 {
     public enum UnitType
@@ -12,10 +16,21 @@ namespace Spineless.Entities
         Boss
     }
 
-    public class Unit : SpinelessEntity
+    public class Unit : SpinelessEntity, IMoveable
     {
         public UnitType UnitType;
         public bool IsAdded;
 
+        #region IMoveable
+
+        public Vector2 Heading { get; set; }
+        public float Speed { get; set; }
+
+        public void MoveTowards()
+        {
+            physics.ApplyForceToBody(0, Speed, Heading);
+        }
+
+        #endregion
     }
 }
